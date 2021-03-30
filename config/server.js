bb07f3446ad1259d3f9419fb2b3cbdfab0f29c48
Caleb Hyatt <caleb@calebhyatt.com>
2021-03-30 11:02:47 -0400

    Update database config.

--- config/server.js
+++ config/server.js
@@ -1,9 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '8c138357c06a40b6f15cd4fcd87e2606'),
-    },
-  },
+  url: env("HEROKU_URL"),
 });
